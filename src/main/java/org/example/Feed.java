06e47d73f1255802f6cff6aadc7e8b551b59b776
moiniom <richard.griesshammer@igsff-bs.de>
2024-07-04 10:19:30 +0200

    Added Feed Class

---
+++ src/main/java/org/example/Feed.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Feed {
+    public Feed(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+    public final String name;
+    public final int price;
+}
