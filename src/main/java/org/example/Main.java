581333d9d7be7eb27e61a468b827f2dba097a9b6
moiniom <richard.griesshammer@igsff-bs.de>
2024-07-04 14:16:41 +0200

    Code Formating Improvement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,15 +13,15 @@ public class Main {
                         new Kangaroo(20, 100)
                 }
         );
-        Farm mollys = new Farm(
-                "Mollys Farm",
+        Farm mollies = new Farm(
+                "Mollies Farm",
                 new Animal[]{
                         new Dog("Jack", 8, 30),
                         new Pig("Jules", 10, 180)
                 }
         );
         //Creates an Array of said farms
-        Farm[] farms = new Farm[]{billies, mollys};
+        Farm[] farms = new Farm[]{billies, mollies};
         //Starts the UI and points it towards the Array
         FarmUI ui = new FarmUI(farms);
     }
